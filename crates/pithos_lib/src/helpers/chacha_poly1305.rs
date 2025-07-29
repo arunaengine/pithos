95a023cd2d1ec7f51bab888feea8da7892d81478
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-07-29 09:49:06 +0200

    refactor: Simplify payload creation in decrypt_chunk function

--- crates/pithos_lib/src/helpers/chacha_poly1305.rs
+++ crates/pithos_lib/src/helpers/chacha_poly1305.rs
@@ -52,7 +52,7 @@ pub fn encrypt_chunk(msg: &[u8], aad: &[u8], enc: &[u8]) -> Result<Bytes, ChaCha
 pub fn decrypt_chunk(
     chunk: &[u8],
     decryption_key: &[u8; 32],
-) -> Result<Bytes, ChaChaPoly1305Error> {
+) -> Result<Vec<u8>, ChaChaPoly1305Error> {
     if chunk.len() < 15 {
         return Err(ChaChaPoly1305Error::ChunkTooSmall(chunk.len()));
     }
@@ -63,59 +63,13 @@ pub fn decrypt_chunk(
         return Err(ChaChaPoly1305Error::InvalidNonce(nonce_slice.len()));
     }
 
-    let last_4 = {
-        let (l1, rem) = data.split_last().unwrap_or((&0u8, &[0u8]));
-        let (l2, rem) = rem.split_last().unwrap_or((&0u8, &[0u8]));
-        let (l3, rem) = rem.split_last().unwrap_or((&0u8, &[0u8]));
-        let (l4, _) = rem.split_last().unwrap_or((&0u8, &[0u8]));
-        (l4, l3, l2, l1)
-    };
-
-    // Padding definition
-    // Encryption with padding must ensure that MAC does not end with 0x00
-    // Padding is signaled by a 0x00 byte in the end, followed by the number of padding 0x00 bytes
-    // <data_ends_with_MAC: ...0abc01230a><padding: 0x0000000000000><padsize (u16): 0x0000><sentinel: 0x00>
-    // Special cases: 1, 2, 3 0x00
-    let mut padding;
-
-    let payload = match last_4 {
-        (0u8, size1, size2, 0u8) => {
-            let expected = [*size1, *size2];
-            let v = BigEndian::read_u16(&expected);
-            if v > 4 {
-                padding = vec![0u8; v as usize - 4];
-                padding.extend_from_slice(&[0u8, *size1, *size2, 0u8]);
-                Payload {
-                    msg: &data[..data.len() - v as usize],
-                    aad: &padding,
-                }
-            } else {
-                Payload {
-                    msg: data,
-                    aad: b"",
-                }
-            }
-        }
-        (_, 0u8, 0u8, 0u8) => Payload {
-            msg: &data[..data.len() - 3],
-            aad: &[0u8, 0u8, 0u8],
-        },
-        (_, _, 0u8, 0u8) => Payload {
-            msg: &data[..data.len() - 2],
-            aad: &[0u8, 0u8],
-        },
-        (_, _, _, 0u8) => Payload {
-            msg: &data[..data.len() - 1],
-            aad: &[0u8],
-        },
-        _ => Payload {
-            msg: data,
-            aad: b"",
-        },
-    };
-
     let cipher = ChaCha20Poly1305::new_from_slice(decryption_key)
         .map_err(|e| ChaChaPoly1305Error::CipherInitError(e.to_string()))?;
+
+    let payload = Payload {
+        msg: data,
+        aad: b"",
+    };
 
     cipher
         .decrypt(nonce_slice.into(), payload)
