0588b10c0176d575ad647e616722343ddd4a6176
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-07-30 14:13:43 +0200

    refactor: Extract PithosWriter functionality in individual functions

--- crates/pithos_lib/src/io/pithoswriter.rs
+++ crates/pithos_lib/src/io/pithoswriter.rs
@@ -1,17 +1,12 @@
-use bytes::BytesMut;
-use fastcdc::v2020::StreamCDC;
-use rand_core::OsRng;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::Write;
-use std::os::unix::fs::PermissionsExt;
-use std::time::SystemTime;
-use x25519_dalek::{PublicKey, StaticSecret};
-
+use crate::helpers::chacha_poly1305::ChaChaPoly1305Error;
+use crate::helpers::directory::DirectoryBuilder;
 use crate::helpers::hash::Hasher;
+use crate::helpers::x25519_keys::CryptError;
+use crate::helpers::zstd::{ZstdError, map_to_zstd_level};
+use crate::model::serialization::SerializationError;
 use crate::model::structs::{
-    BlockDataState, BlockHeader, BlockIndexEntry, BlockLocation, Directory, EncryptionSection,
-    FileEntry, FileType, ProcessingFlags, RecipientData, RecipientSection,
+    BlockHeader, BlockIndexEntry, BlockLocation, Directory, EncryptionSection, FileEntry,
+    ProcessingFlags, RecipientData, RecipientSection,
 };
 use crate::{
     helpers::{
@@ -61,153 +56,128 @@ pub struct FileWithMetadata {
 }
 
 pub struct PithosWriter {
-    writer_key: StaticSecret,
-    reader_keys: Vec<PublicKey>,
-    files: Vec<FileWithMetadata>,
+    // Input
+    writer_key: StaticSecret, //TODO: Multiple sender keys for individual EncryptionSections
+    reader_keys: Vec<PublicKey>, //TODO: Individual assignment of files to recipients
     sink: Box<dyn Write>,
+
+    // Processing
+    directory: Directory,
+    chunk_idx: u64,
+    file_idx: u64,
+    written_bytes: u64,
 }
 
 impl PithosWriter {
     pub fn new(
         writer_key: StaticSecret,
         reader_keys: Vec<PublicKey>,
-        files: Vec<FileWithMetadata>,
         sink: Box<dyn Write>,
-    ) -> Self {
-        PithosWriter {
+    ) -> Result<Self, PithosWriterError> {
+        // Init encryption section
+        let encryption_sections = vec![EncryptionSection {
+            sender_public_key: PublicKey::from(&writer_key).to_bytes(),
+            recipients: reader_keys
+                .iter()
+                .map(|key| RecipientSection {
+                    recipient_public_key: key.to_bytes(),
+                    recipient_data: RecipientData::Decrypted(Vec::new()),
+                })
+                .collect(),
+        }];
+
+        Ok(PithosWriter {
             writer_key,
             reader_keys,
-            files,
             sink,
-        }
+            directory: DirectoryBuilder::new()
+                .encryption(encryption_sections)
+                .build()?,
+            chunk_idx: 0,
+            file_idx: 0,
+            written_bytes: 0,
+        })
     }
 
-    pub fn run(&mut self) -> anyhow::Result<()> {
+    pub fn new_from_file() -> anyhow::Result<Self> {
+        //TODO: Read directory from file
+        //TODO: Do stuff
+        todo!()
+    }
+
+    // Create a new Pithos file from the input
+    pub fn process_files(&mut self, files: Vec<FileWithMetadata>) -> Result<(), PithosWriterError> {
         // Write file header
-        let file_header = FileHeader::default().serialize_to_bytes()?;
-        self.sink.write_all(&file_header)?;
+        self.write_file_header()?;
 
-        // Init directory and hashmap for easier EncryptionSection collection
-        let mut directory = Directory::new()?;
-        let mut encryption_sections: HashMap<[u8; 32], Vec<RecipientSection>> =
-            HashMap::from_iter([(
-                self.writer_key.to_bytes(),
-                self.reader_keys
-                    .iter()
-                    .map(|k| RecipientSection {
-                        recipient_public_key: k.to_bytes(),
-                        recipient_data: RecipientData::Decrypted(Vec::new()),
-                    })
-                    .collect(),
-            )]);
-
-        // Write blocks and update directory
-        let mut file_index = 0u64;
-        let mut chunk_index = 0u64;
-        let mut written_bytes = file_header.len() as u64;
-
-        for file in &mut self.files {
+        // Write files in CDC blocks
+        for file in files.iter() {
             let file_handle = File::open(&file.file_path)?;
             let file_metadata = file_handle.metadata()?;
-            let mut file_entry = FileEntry {
-                file_id: file_index,
-                path: file.file_name.clone(),
-                file_type: FileType::Data, //TODO: Directory ingestion
-                block_data: BlockDataState::Decrypted(vec![]),
-                created: file_metadata
-                    .created()
-                    .unwrap_or(SystemTime::UNIX_EPOCH)
-                    .duration_since(SystemTime::UNIX_EPOCH)?
-                    .as_secs(),
-                modified: file_metadata
-                    .modified()
-                    .unwrap_or(SystemTime::UNIX_EPOCH)
-                    .duration_since(SystemTime::UNIX_EPOCH)?
-                    .as_secs(),
-                file_size: file_metadata.len(),
-                permissions: file_metadata.permissions().mode(),
-                references: vec![],
-                symlink_target: None,
-            };
+            let mut file_entry = FileEntry::new(self.file_idx, &file.file_path, file_metadata)
+                .map_err(|e| PithosWriterError::Other(e.to_string()))?;
+            let flags = ProcessingFlags::new(file.encrypted, file.compression_level);
 
-            let mut fastcdc_stream = StreamCDC::with_level(
+            // Init FastCDC stream
+            //TODO: Configurable
+            let fastcdc_stream = StreamCDC::with_level(
                 file_handle,
                 65536,   //1024,
                 262144,  //4096,
                 1048576, //16384,
                 fastcdc::v2020::Normalization::Level1,
             );
-            while let Some(res) = fastcdc_stream.next() {
-                match res {
-                    Ok(chunk) => {
-                        // Create mutable version of chunk data
-                        let mut processed_chunk = BytesMut::from(chunk.data.as_slice());
 
-                        // Compress chunk
-                        let chunk_sample = if chunk.data.len() > 8192 {
-                            &chunk.data[..8192]
-                        } else {
-                            &chunk.data
-                        };
-                        if probe_compression_ratio(&chunk_sample, None)
-                            .map_err(|e| anyhow::anyhow!("Compression probe failed: {}", e))?
-                            < 0.85
-                        {
-                            processed_chunk = BytesMut::from(
-                                compress_data(&chunk.data, None)
-                                    .map_err(|e| anyhow::anyhow!("Compression failed: {}", e))?
-                                    .as_slice(),
-                            );
-                        }
+            // Iterate over CDC blocks
+            for result in fastcdc_stream {
+                let mut chunk = result?;
 
-                        // Calculate chunk hash
-                        let mut hasher = Hasher::new();
-                        hasher.update(&processed_chunk);
-                        let hashes = hasher.finalize();
-                        processed_chunk = encrypt_chunk(&processed_chunk, b"", &hashes.shake256)
-                            .map_err(|e| anyhow::anyhow!("Encryption failed: {}", e))?
-                            .into();
+                // Calculate block hashes
+                let mut hasher = Hasher::new();
+                hasher.update(&chunk.data);
+                let hashes = hasher.finalize();
 
-                        // Create BlockIndexEntry
-                        //TODO: Block deduplication -> Iterate over existing chunks and compare hash
-                        let block_index_entry = BlockIndexEntry {
-                            index: chunk_index,
-                            hash: *hashes.blake3.as_bytes(),
-                            offset: written_bytes,
-                            stored_size: processed_chunk.len() as u64,
-                            original_size: chunk.length as u64,
-                            flags: ProcessingFlags(0b0000_1110), //TODO: Configurable/Dynamic compression level
-                            location: BlockLocation::Local,      //TODO: Remote files
-                        };
-
-                        // Write block
-                        BlockHeader::default().serialize(&mut self.sink)?;
-                        self.sink.write_all(&processed_chunk)?;
-                        written_bytes =
-                            written_bytes.saturating_add(4 + processed_chunk.len() as u64);
-
-                        // Modify FileIndexEntry
-                        match file_entry.block_data {
-                            BlockDataState::Encrypted(_) => {
-                                return Err(anyhow::anyhow!("Invalid BlockDataState::Encrypted"))
-                            }
-                            BlockDataState::Decrypted(ref mut blocks) => {
-                                blocks.push((chunk_index, hashes.shake256))
-                            }
-                        }
-
-                        // Add BlockIndexEntry to directory
-                        directory.blocks.push(block_index_entry);
-
-                        // Increment block counter
-                        chunk_index = chunk_index.saturating_add(1);
-                    }
-                    Err(e) => {
-                        //TODO: Error Handling
-                        eprintln!("Error processing chunk: {}", e);
-                        break; // Handle error as needed
-                    }
+                // Check if block already exists
+                if let Some(index) = self.directory.block_exists(hashes.blake3) {
+                    // Add block to FileEntry and continue with next chunk
+                    file_entry.add_block_data((index, hashes.shake256))?;
+                    continue;
                 }
+
+                // Init BlockIndexEntry
+                let mut block_index_entry = BlockIndexEntry {
+                    index: self.chunk_idx,
+                    hash: *hashes.blake3.as_bytes(),
+                    offset: self.written_bytes,
+                    stored_size: chunk.data.len() as u64,
+                    original_size: chunk.length as u64,
+                    flags,
+                    location: BlockLocation::Local, //TODO: Remote files
+                };
+
+                // Compression
+                let compression_level = map_to_zstd_level(flags);
+                if compression_level > 0
+                    && probe_compression_ratio(&chunk.data, Some(compression_level))? < 0.85
+                {
+                    chunk.data = compress_data(chunk.data.as_slice(), None)?;
+                } else {
+                    // No compression. But why? 15% is better than nothing.
+                    block_index_entry.flags.set_compression_level(0);
+                }
+
+                // Encryption
+                if flags.is_encrypted() {
+                    chunk.data = encrypt_chunk(chunk.data.as_slice(), b"", &hashes.shake256)?;
+                }
+
+                // Update stored size to processed block length
+                block_index_entry.stored_size = chunk.data.len() as u64;
+
+                // Write block; Add block to file entry; Add block to index in directory
+                self.write_block(&chunk.data)?;
+                file_entry.add_block_data((block_index_entry.index, hashes.shake256))?;
+                self.directory.add_block_to_index(block_index_entry);
             }
 
             // Create random key and encrypt file block index
@@ -215,55 +185,77 @@ impl PithosWriter {
             file_entry.block_data.encrypt(enc_key)?;
 
             // Add file entry to directory
-            directory.files.push(file_entry);
-
-            // Add to (currently all) recipient sections
-            //TODO: Populate different encryption sections individually
-            if let Some(sections) = encryption_sections.get_mut(&self.writer_key.to_bytes()) {
-                for section in sections {
-                    match section.recipient_data {
-                        RecipientData::Encrypted(_) => {
-                            return Err(anyhow::anyhow!("Invalid RecipientData state."))
-                        }
-                        RecipientData::Decrypted(ref mut entries) => {
-                            entries.push((file_index, enc_key))
-                        }
-                    }
-                }
-            }
+            self.directory.add_file_to_index(file_entry);
+            self.directory
+                .add_file_to_all_recipients((self.file_idx, enc_key));
 
             // Increment file index
-            file_index = file_index.saturating_add(1);
+            self.file_idx = self.file_idx.saturating_add(1);
         }
 
-        // Encrypt recipient data and integrate into directory
-        //TODO: Multiple sender keys
-        if let Some(sections) = encryption_sections.get_mut(&self.writer_key.to_bytes()) {
-            // Create encryption section for sender key
-            let mut enc_section = EncryptionSection {
-                sender_public_key: PublicKey::from(&self.writer_key).to_bytes(),
-                recipients: vec![],
-            };
-
-            // Add recipient sections to encryption section
-            for section in sections {
-                section.encrypt(&self.writer_key)?;
-                enc_section.recipients.push(section.clone());
-            }
-
-            // Add encryption section to directory
-            directory.encryption.push(enc_section);
-        }
+        // Encrypt recipient data
+        self.directory.encrypt_recipients(&self.writer_key)?;
 
         // Finish directory and write into sink
-        directory.update_len()?;
-        directory.update_crc32()?;
-        directory.serialize(&mut self.sink)?;
+        self.directory.update_len()?;
+        self.directory.update_crc32()?;
+        self.directory.serialize(&mut self.sink)?;
 
         Ok(())
     }
 
-    pub fn write_header() -> anyhow::Result<()> {
+    // Append to an existing Pithos file
+    // Should only be used after existing pithos file was loaded:
+    //   - Omits the file header
+    //   - Writes a new directory with offset to parent directory
+    pub fn append_files(&mut self) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    pub fn write_file_header(&mut self) -> Result<(), SerializationError> {
+        FileHeader::default().serialize(&mut self.sink)?;
+        self.written_bytes = self.written_bytes.saturating_add(6);
+        Ok(())
+    }
+
+    pub fn compress_block(
+        &self,
+        mut block: Vec<u8>,
+        compression_level: u8,
+    ) -> Result<Vec<u8>, ZstdError> {
+        // Compress chunk
+        let chunk_sample = if block.len() > 8192 {
+            &block[..8192]
+        } else {
+            &block
+        };
+
+        if probe_compression_ratio(&chunk_sample, Some(compression_level as i32))? < 0.85 {
+            block = compress_data(&block, Some(compression_level as i32))?;
+        }
+        Ok(block)
+    }
+
+    pub fn write_block(&mut self, block: &[u8]) -> Result<(), SerializationError> {
+        // Write BlockHeader
+        BlockHeader::default().serialize(&mut self.sink)?;
+        self.written_bytes = self.written_bytes.saturating_add(4); // Add BlockHeader length
+
+        // Write block data
+        self.sink.write_all(block)?;
+        self.written_bytes = self.written_bytes.saturating_add(block.len() as u64);
+
+        Ok(())
+    }
+
+    pub fn write_directory(&mut self) -> Result<(), SerializationError> {
+        // Update len and crc32
+        self.directory.update_len()?;
+        self.directory.update_crc32()?;
+
+        // Write directory
+        self.directory.serialize(&mut self.sink)?;
+
         Ok(())
     }
 }
