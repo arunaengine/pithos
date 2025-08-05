706f52b89dcc43c808f1da3833c0fbb8b0d0de37
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-08-05 12:33:33 +0200

    tests: Adapt to changes

--- crates/pithos_lib/tests/reader.rs
+++ crates/pithos_lib/tests/reader.rs
@@ -1,15 +1,55 @@
 #[cfg(test)]
 mod tests {
     use pithos_lib::io::pithosreader::PithosReaderSimple;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_reader_single_file() {
+        let pithos_file = "/tmp/file.pith".to_string();
+        let key_pem = "tests/data/recipient1_private.pem".to_string();
+
+        let mut reader = PithosReaderSimple::new(pithos_file, key_pem).unwrap();
+        let directory = reader.read_directory().unwrap();
+
+        dbg!(reader.read_file_paths(&directory).unwrap());
+    }
+
+    #[test]
+    fn test_reader_single_file_range() {
+        let pithos_file = "/tmp/file.pith".to_string();
+        let key_pem = "tests/data/recipient1_private.pem".to_string();
+        let output_path = PathBuf::from("/tmp/output/");
+
+        let mut reader = PithosReaderSimple::new(pithos_file, key_pem).unwrap();
+        let directory = reader.read_directory().unwrap();
+
+        if let Some(entry) = directory.get_file_entry("t8.shakespeare.txt") {
+            reader
+                .read_file(
+                    &entry.path,
+                    &directory,
+                    Some(&output_path),
+                    Some(12000..200000),
+                )
+                .unwrap()
+        }
+    }
 
     #[test]
     fn test_reader() {
-        let pithos_file = "/tmp/test.pith".to_string();
+        let pithos_file = "/tmp/directory.pith".to_string();
+        let output_directory = "/tmp/output/";
         let key_pem = "tests/data/recipient1_private.pem".to_string();
 
-        let mut pithos_reader = PithosReaderSimple::new(pithos_file, key_pem).unwrap();
+        let mut reader = PithosReaderSimple::new(pithos_file, key_pem).unwrap();
+        let directory = reader.read_directory().unwrap();
 
-        let _dir = pithos_reader.read_directory().unwrap();
-        //println!("{:#?}", dir)
+        // Write all files available in the directory
+        let output_path = PathBuf::from(output_directory);
+        for inner_file in &directory.files {
+            reader
+                .read_file(&inner_file.path, &directory, Some(&output_path), None)
+                .unwrap();
+        }
     }
 }
--- crates/pithos_lib/tests/writer.rs
+++ crates/pithos_lib/tests/writer.rs
@@ -1,18 +1,36 @@
 #[cfg(test)]
 mod tests {
-    use pithos_lib::io::pithoswriter::PithosWriter;
+    use pithos_lib::io::pithoswriter::{FileMeta, InputFile, PithosWriter};
+    use pithos_lib::model::structs::FileType;
     use std::fs::File;
 
     #[test]
-    fn test_writer() {
+    fn test_writer_single_file() {
         // Dummy file
-        let file_with_metadata = pithos_lib::io::pithoswriter::FileWithMetadata {
-            file_path: "/tmp/sample.file".to_string(),
-            file_name: "sample.file".to_string(),
-            file_metadata: "{'foo': 'bar'}".to_string(),
-            encrypted: true,
-            compression_level: Some(3),
+        let input_file = InputFile {
+            file_path: "/tmp/t8.shakespeare.txt".to_string(),
+            file_type: FileType::Data,
+            file_meta: None,
+            encrypt: true,
+            compression_level: Some(7),
         };
+        /*
+        let input_file = InputFile {
+            file_path: "/tmp/SRR33138449.fastq".to_string(),
+            file_type: FileType::Directory,
+            file_meta: Some(FileMeta::Content(
+                r#"{
+  "@id": "SRR33138449.fastq",
+  "@type": "File",
+  "name": "SRR33138449 run sequencing reads",
+  "description": "Something something description",
+}"#
+                .to_string(),
+            )),
+            encrypt: true,
+            compression_level: Some(1),
+        };
+        */
 
         // Read sender private key
         let sender_pem_content = std::fs::read_to_string("tests/data/sender_private.pem").unwrap();
@@ -31,12 +49,102 @@ mod tests {
 
         // Prepare input for writer
         let reader_keys = vec![reader_key];
-        let outfile = File::create("/tmp/test.pith").unwrap();
+        let outfile = File::create("/tmp/file.pith").unwrap();
 
         let mut writer = PithosWriter::new(writer_key, reader_keys, Box::new(outfile)).unwrap();
 
-        // Process files
-        //TODO: Provide config / file to key association
-        writer.process_files(vec![file_with_metadata]).unwrap();
+        // Process
+        writer.write_file_header().unwrap();
+        writer.process_input(input_file, Some("/tmp/")).unwrap();
+        writer.write_directory().unwrap();
     }
+
+    #[test]
+    fn test_writer_multiple_files() {
+        // Dummy file
+        let input_files = vec![
+            InputFile {
+                file_path: "/tmp/SRR33138449.fastq".to_string(),
+                file_type: FileType::Directory,
+                file_meta: Some(FileMeta::Content(
+                    r#"{
+  "@id": "SRR33138449.fastq",
+  "@type": "File",
+  "name": "SRR33138449 run sequencing reads",
+  "description": "Something something description",
+}"#
+                    .to_string(),
+                )),
+                encrypt: true,
+                compression_level: Some(1),
+            },
+            InputFile {
+                file_path: "/tmp/t8.shakespeare.txt".to_string(),
+                file_type: FileType::Data,
+                file_meta: Some(FileMeta::Content(
+                    r#"{"@id": "t8.shakespeare.txt","@type": "File"}"#.to_string(),
+                )),
+                encrypt: true,
+                compression_level: Some(7),
+            },
+        ];
+
+        // Read sender private key
+        let sender_pem_content = std::fs::read_to_string("tests/data/sender_private.pem").unwrap();
+        let writer_key = pithos_lib::helpers::x25519_keys::private_key_from_pem_bytes(
+            sender_pem_content.as_bytes(),
+        )
+        .unwrap();
+
+        // Read recipient public key
+        let recipient_pem_content =
+            std::fs::read_to_string("tests/data/recipient1_public.pem").unwrap();
+        let reader_key = pithos_lib::helpers::x25519_keys::public_key_from_pem_bytes(
+            recipient_pem_content.as_bytes(),
+        )
+        .unwrap();
+
+        // Prepare input for writer
+        let reader_keys = vec![reader_key];
+        let outfile = File::create("/tmp/files.pith").unwrap();
+
+        let mut writer = PithosWriter::new(writer_key, reader_keys, Box::new(outfile)).unwrap();
+
+        // Process
+        writer.write_file_header().unwrap();
+        writer.process_input_files(input_files).unwrap();
+        writer.write_directory().unwrap();
+    }
+
+    #[test]
+    fn test_directory_writer() {
+        // Read sender private key
+        let sender_pem_content = std::fs::read_to_string("tests/data/sender_private.pem").unwrap();
+        let writer_key = pithos_lib::helpers::x25519_keys::private_key_from_pem_bytes(
+            sender_pem_content.as_bytes(),
+        )
+        .unwrap();
+
+        // Read recipient public key
+        let recipient_pem_content =
+            std::fs::read_to_string("tests/data/recipient1_public.pem").unwrap();
+        let reader_key = pithos_lib::helpers::x25519_keys::public_key_from_pem_bytes(
+            recipient_pem_content.as_bytes(),
+        )
+        .unwrap();
+
+        // Prepare input for writer
+        let reader_keys = vec![reader_key];
+        let outfile = File::create("/tmp/directory.pith").unwrap();
+
+        let mut writer = PithosWriter::new(writer_key, reader_keys, Box::new(outfile)).unwrap();
+
+        // Process directory
+        writer.write_file_header().unwrap();
+        writer.process_directory("/tmp/demo-results/").unwrap();
+        writer.write_directory().unwrap();
+    }
+
+    #[test]
+    fn test_append_writer() {}
 }
