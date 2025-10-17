87a350541c903427e95e19e97829ae7597504927
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-10-17 16:15:26 +0200

    tests: Extend and improve ChaChaResilient tests

--- crates/pithos_lib/src/lib.rs
+++ crates/pithos_lib/src/lib.rs
@@ -21,7 +21,7 @@ mod tests {
     use crate::transformers::decrypt::ChaCha20Dec;
     use crate::transformers::decrypt_resilient::ChaChaResilient;
     use crate::transformers::decrypt_with_parts::ChaCha20DecParts;
-    use crate::transformers::encrypt::ChaCha20Enc;
+    use crate::transformers::encrypt::{encrypt_chunk, ChaCha20Enc};
     use crate::transformers::filter::Filter;
     use crate::transformers::footer::FooterGenerator;
     use crate::transformers::footer_extractor::FooterExtractor;
@@ -37,6 +37,7 @@ mod tests {
     use digest::Digest;
     use futures::{StreamExt, TryStreamExt};
     use md5::Md5;
+    use tempfile::TempDir;
     use tokio::fs::File;
     use tokio::io::{AsyncReadExt, AsyncSeekExt};
 
@@ -1308,14 +1309,21 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn e2e_test_resilient_parts_decryptor() {
-        let file = File::open("test.txt").await.unwrap();
-        let file2 = vec![];
+    async fn e2e_test_resilient_decryptor_no_lengths() {
+        // Encrypt test file
+        let temp_dir = TempDir::new().unwrap();
+        let enc_file_path = temp_dir.path().join("test.txt.enc");
 
-        let repeated: Vec<u64> = vec![50860];
+        let file_in = File::open("test.txt").await.unwrap();
+        let mut enc_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&enc_file_path)
+            .await
+            .unwrap();
 
-        // Create a new GenericReadWriter
-        GenericReadWriter::new_with_writer(file, file2)
+        GenericReadWriter::new_with_writer(file_in, &mut enc_out)
             .add_transformer(
                 ChaCha20Enc::new_with_fixed(
                     b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
@@ -1325,15 +1333,384 @@ mod tests {
                 )
                 .unwrap(),
             )
+            .process()
+            .await
+            .unwrap();
+
+        // Read encrypted parts in buffer
+        let mut input = Vec::new();
+        let mut enc_file = File::open(enc_file_path).await.unwrap();
+        enc_file.read_to_end(&mut input).await.unwrap();
+
+        // Create a new GenericReadWriter that decrypts the parts
+        let mut output = vec![];
+        let part_lengths: Vec<u64> = vec![];
+        GenericReadWriter::new_with_writer(input.as_slice(), &mut output)
             .add_transformer(ChaChaResilient::new_with_lengths(
                 b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
                     .to_vec()
                     .try_into()
                     .unwrap(),
-                repeated,
+                part_lengths,
             ))
             .process()
             .await
             .unwrap();
+
+        // Assert output is same as original file
+        let mut original_file = File::open("test.txt").await.unwrap();
+        let file_size = original_file.metadata().await.unwrap().len();
+        assert_eq!(output.len() as u64, file_size);
+
+        let mut original_bytes = Vec::new();
+        original_file
+            .read_to_end(&mut original_bytes)
+            .await
+            .unwrap();
+        assert_eq!(output, original_bytes);
+    }
+
+    #[tokio::test]
+    async fn e2e_test_resilient_decryptor_single() {
+        // Encrypt single part
+        let temp_dir = TempDir::new().unwrap();
+
+        let part_1_path = temp_dir.path().join("test.1.txt.enc");
+        let part_1_in = File::open("test.1.txt").await.unwrap();
+        let mut part_1_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&part_1_path)
+            .await
+            .unwrap();
+
+        GenericReadWriter::new_with_writer(part_1_in, &mut part_1_out)
+            .add_transformer(
+                ChaCha20Enc::new_with_fixed(
+                    b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                        .to_vec()
+                        .try_into()
+                        .unwrap(),
+                )
+                .unwrap(),
+            )
+            .process()
+            .await
+            .unwrap();
+
+        // Read encrypted part in buffer
+        let mut input = Vec::new();
+        let mut file_part_1 = File::open(part_1_path).await.unwrap();
+        file_part_1.read_to_end(&mut input).await.unwrap();
+
+        let mut output = vec![];
+        let part_lengths: Vec<u64> = vec![4605];
+
+        // Create a new GenericReadWriter that decrypts the part
+        GenericReadWriter::new_with_writer(input.as_slice(), &mut output)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                part_lengths,
+            ))
+            .process()
+            .await
+            .unwrap();
+
+        // Assert output is same as original file
+        let mut original_file = File::open("test.1.txt").await.unwrap();
+        let file_size = original_file.metadata().await.unwrap().len();
+        assert_eq!(output.len() as u64, file_size);
+
+        let mut original_bytes = Vec::new();
+        original_file
+            .read_to_end(&mut original_bytes)
+            .await
+            .unwrap();
+        assert_eq!(output, original_bytes);
+    }
+
+    #[tokio::test]
+    async fn e2e_test_resilient_decryptor_multi() {
+        // Create temp paths for encrypted parts
+        let temp_dir = TempDir::new().unwrap();
+        let part_1_path = temp_dir.path().join("test.1.txt.enc");
+        let part_2_path = temp_dir.path().join("test.2.txt.enc");
+
+        // Encrypt parts individually
+        let part_1_in = File::open("test.1.txt").await.unwrap();
+        let part_1_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&part_1_path)
+            .await
+            .unwrap();
+
+        let part_2_in = File::open("test.2.txt").await.unwrap();
+        let part_2_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&part_2_path)
+            .await
+            .unwrap();
+
+        for (input, mut output) in vec![(part_1_in, part_1_out), (part_2_in, part_2_out)] {
+            GenericReadWriter::new_with_writer(input, &mut output)
+                .add_transformer(
+                    ChaCha20Enc::new_with_fixed(
+                        b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                            .to_vec()
+                            .try_into()
+                            .unwrap(),
+                    )
+                    .unwrap(),
+                )
+                .process()
+                .await
+                .unwrap();
+        }
+
+        // Read encrypted parts in buffer
+        let mut input = Vec::new();
+        let mut file_part_1 = File::open(part_1_path).await.unwrap();
+        file_part_1.read_to_end(&mut input).await.unwrap();
+        let mut file_part_2 = File::open(part_2_path).await.unwrap();
+        file_part_2.read_to_end(&mut input).await.unwrap();
+
+        let mut output = vec![];
+        let part_lengths: Vec<u64> = vec![4605, 46283];
+
+        // Create a new GenericReadWriter that decrypts the parts
+        GenericReadWriter::new_with_writer(input.as_slice(), &mut output)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                part_lengths,
+            ))
+            .process()
+            .await
+            .unwrap();
+
+        // Assert output is same as original file
+        let mut original_file = File::open("test.txt").await.unwrap();
+        let file_size = original_file.metadata().await.unwrap().len();
+        assert_eq!(output.len() as u64, file_size);
+
+        let mut original_bytes = Vec::new();
+        original_file
+            .read_to_end(&mut original_bytes)
+            .await
+            .unwrap();
+        assert_eq!(output, original_bytes);
+    }
+
+    #[tokio::test]
+    async fn e2e_test_resilient_decryptor_multi_with_compression() {
+        println!("Moin.");
+        // Create temp paths for encrypted parts
+        let temp_dir = TempDir::new().unwrap();
+        let part_1_path = temp_dir.path().join("test.1.txt.enc");
+        let part_2_path = temp_dir.path().join("test.2.txt.enc");
+
+        // Encrypt parts individually
+        let part_1_in = File::open("test.1.txt").await.unwrap();
+        let part_1_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&part_1_path)
+            .await
+            .unwrap();
+
+        let part_2_in = File::open("test.2.txt").await.unwrap();
+        let part_2_out = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&part_2_path)
+            .await
+            .unwrap();
+
+        for (input, mut output) in vec![(part_1_in, part_1_out), (part_2_in, part_2_out)] {
+            GenericReadWriter::new_with_writer(input, &mut output)
+                .add_transformer(ZstdEnc::new())
+                .add_transformer(
+                    ChaCha20Enc::new_with_fixed(
+                        b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                            .to_vec()
+                            .try_into()
+                            .unwrap(),
+                    )
+                    .unwrap(),
+                )
+                .process()
+                .await
+                .unwrap();
+        }
+
+        // Read encrypted parts in buffer
+        let mut input = Vec::new();
+        let mut file_part_1 = File::open(part_1_path).await.unwrap();
+        let file_part_1_size = file_part_1.metadata().await.unwrap().len();
+        file_part_1.read_to_end(&mut input).await.unwrap();
+
+        let mut file_part_2 = File::open(part_2_path).await.unwrap();
+        let file_part_2_size = file_part_2.metadata().await.unwrap().len();
+        file_part_2.read_to_end(&mut input).await.unwrap();
+
+        let mut output = vec![];
+        let part_lengths: Vec<u64> = vec![file_part_1_size % 65564, file_part_2_size % 65564]; //vec![4605, 46283];
+
+        // Create a new GenericReadWriter that decrypts the parts
+        GenericReadWriter::new_with_writer(input.as_slice(), &mut output)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                part_lengths,
+            ))
+            .add_transformer(ZstdDec::new())
+            .process()
+            .await
+            .unwrap();
+
+        // Assert output is same as original file
+        let mut original_file = File::open("test.txt").await.unwrap();
+        let file_size = original_file.metadata().await.unwrap().len();
+        assert_eq!(output.len() as u64, file_size);
+
+        let mut original_bytes = Vec::new();
+        original_file
+            .read_to_end(&mut original_bytes)
+            .await
+            .unwrap();
+        assert_eq!(output, original_bytes);
+    }
+
+    #[tokio::test]
+    async fn transformer_output_comparison() {
+        let original_file = File::open("test.txt").await.unwrap();
+        let file_size = original_file.metadata().await.unwrap().len();
+
+        // Compress and encrypt with ZstdEnc+ChaChaEnc
+        let mut comp_enc_out = Vec::new();
+        GenericReadWriter::new_with_writer(original_file, &mut comp_enc_out)
+            .add_transformer(ZstdEnc::new())
+            .add_transformer(
+                ChaCha20Enc::new_with_fixed(
+                    b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                        .to_vec()
+                        .to_vec()
+                        .try_into()
+                        .unwrap(),
+                )
+                .unwrap(),
+            )
+            .process()
+            .await
+            .unwrap();
+
+        // Compress and encrypt with PithosTransformer
+        let (sx, rx) = async_channel::bounded(10);
+        sx.send(Message::FileContext(FileContext {
+            file_path: "test.txt".to_string(),
+            compressed_size: file_size,
+            decompressed_size: file_size,
+            recipients_pubkeys: vec![],
+            encryption_key: EncryptionKey::Same(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+            ),
+            ..Default::default()
+        }))
+        .await
+        .unwrap();
+
+        let original_file = File::open("test.txt").await.unwrap();
+        let mut pithos_out = Vec::new();
+        let mut aswr = GenericReadWriter::new_with_writer(original_file, &mut pithos_out)
+            .add_transformer(PithosTransformer::new());
+        aswr.add_message_receiver(rx).await.unwrap();
+        aswr.process().await.unwrap();
+
+        drop(aswr);
+        //assert_eq!(comp_enc_out.len(), pithos_out.len())
+
+        let mut decrypted1 = Vec::new();
+        // Create a new GenericReadWriter that decrypts the parts
+        GenericReadWriter::new_with_writer(comp_enc_out.as_slice(), &mut decrypted1)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                vec![comp_enc_out.len() as u64],
+            ))
+            .add_transformer(ZstdDec::new())
+            .process()
+            .await
+            .unwrap();
+        assert_eq!(file_size, decrypted1.len() as u64);
+
+        let mut decrypted2 = Vec::new();
+        // Create a new GenericReadWriter that decrypts the parts
+        GenericReadWriter::new_with_writer(pithos_out.as_slice(), &mut decrypted2)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                vec![pithos_out.len() as u64],
+            ))
+            .add_transformer(ZstdDec::new())
+            .process()
+            .await
+            .unwrap();
+        assert_eq!(file_size, decrypted2.len() as u64);
+
+        assert_eq!(decrypted1, decrypted2);
+    }
+
+    #[tokio::test]
+    async fn e2e_test_resilient_decryptor_single_chunk() {
+        // Encrypt test file in single chunk
+        let mut file_in = File::open("test.txt").await.unwrap();
+        let mut file_bytes = Vec::new();
+        file_in.read_to_end(&mut file_bytes).await.unwrap();
+
+        let encrypted_bytes =
+            encrypt_chunk(&file_bytes, b"", b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea", false).unwrap();
+
+        println!("{}", encrypted_bytes.len());
+
+        // Create a new GenericReadWriter that decrypts the parts
+        let mut output = vec![];
+        let part_lengths: Vec<u64> = vec![encrypted_bytes.len() as u64]; //vec![5099288];
+        GenericReadWriter::new_with_writer(encrypted_bytes.to_vec().as_slice(), &mut output)
+            .add_transformer(ChaChaResilient::new_with_lengths(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+                part_lengths,
+            ))
+            .process()
+            .await
+            .unwrap();
+
+        // Assert output is same as original file
+        assert_eq!(file_bytes.len(), output.len());
+        assert_eq!(output, file_bytes);
     }
 }
